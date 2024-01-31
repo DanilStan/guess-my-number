1550e8f8210281d99699e03a64f35ced5515d31f
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-31 23:10:48 +0200

    Create refs

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const refs = {
+  inputRef: document.querySelector('input'),
+  btnCheck: document.querySelector('.left__btn'),
+  btnAgain: document.querySelector('.header__btn'),
+  scoreValue: document.querySelector('.score'),
+  highScoreValue: document.querySelector('.highscore'),
+  infoText: document.querySelector('.left__start'),
+};
+
+console.log(refs);
