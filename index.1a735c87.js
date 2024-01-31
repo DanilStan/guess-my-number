3349a1217308cf774cd7221441abf2c1806e1baf
DanilStan <stanislavov.danik@gmail.com>
2024-01-31 21:11:24 +0000

    Deploying to gh-pages from @ DanilStan/guess-my-number@ab0138a7c5f47faebb4c2e0977dfcd2d9c02241a 🚀

---
+++ index.1a735c87.js
@@ -0,0 +1,2 @@
+const e={inputRef:document.querySelector("input"),btnCheck:document.querySelector(".left__btn"),btnAgain:document.querySelector(".header__btn"),scoreValue:document.querySelector(".score"),highScoreValue:document.querySelector(".highscore"),infoText:document.querySelector(".left__start")};console.log(e);
+//# sourceMappingURL=index.1a735c87.js.map
---
+++ index.9c07acbe.js
@@ -0,0 +1,2 @@
+!function(){var e={inputRef:document.querySelector("input"),btnCheck:document.querySelector(".left__btn"),btnAgain:document.querySelector(".header__btn"),scoreValue:document.querySelector(".score"),highScoreValue:document.querySelector(".highscore"),infoText:document.querySelector(".left__start")};console.log(e)}();
+//# sourceMappingURL=index.9c07acbe.js.map
