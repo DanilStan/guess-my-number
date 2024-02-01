2574d9c0892ee41c2bf74679cf8a6fdb002992ba
DanilStan <stanislavov.danik@gmail.com>
2024-02-01 14:35:54 +0000

    Deploying to gh-pages from @ DanilStan/guess-my-number@4c609e1882554709000eb51598fd2c8af878c678 🚀

--- index.1a735c87.js
+++
@@ -1,2 +0,0 @@
-const e={inputRef:document.querySelector("input"),btnCheck:document.querySelector(".left__btn"),btnAgain:document.querySelector(".header__btn"),scoreValue:document.querySelector(".score"),highScoreValue:document.querySelector(".highscore"),infoText:document.querySelector(".left__start")};console.log(e);
-//# sourceMappingURL=index.1a735c87.js.map
